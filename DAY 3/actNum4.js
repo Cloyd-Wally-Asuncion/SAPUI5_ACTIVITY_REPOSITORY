515e15ddc9a36245e8805e1006aa4703ac2a4b60
Cloyd Wally Asuncion <cloyd.wally.asuncion@accenture.com>
2025-08-14 03:11:49 +0000

    Added Activity Number 4

---
+++ DAY 3/actNum4.js
@@ -0,0 +1,10 @@
+var side1 = 3;
+var side2 = 3;
+var side3 = 4;
+
+function trianglePerimeter(){
+    var perimeter = side1 + side2 + side3;
+    console.log("The perimeter of the triangle is:" + perimeter);
+}
+
+trianglePerimeter();
